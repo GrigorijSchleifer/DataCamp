9cd0ab59127b14646f6bf9e799992ae2ed5b7c8a
Dr. med. Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2023-02-20 07:09:58 +0100

    Jupyter notebook for NLP course created

--- Introduction_to_NLP_in_python/Regex_part.py
+++ Introduction_to_NLP_in_python/Regex_part.py
@@ -1,16 +1,1 @@
-import re
-# to specify function arguments that are function
-from typing import Callable
-
-def read_in_data():
-    with read("data/scene_one")
-
-
-# open the file for appending
-with open("data/scence_one.txt", "r") as f_r:
-    f_w = f_w.readlines()
-
-    with open("data/scene_one") as f_w:
-        for ln in f_w:
-            ln = ln.readlines()
 
